4c9ab0dd364a91a8f5eb3f47525a6bef0ae7bec5
parsyarifqi <parsyapetrana4@gmail.com>
2026-09-14 22:02:53 +0700

    test: added testing for the education model without error

--- main/tests.py
+++ main/tests.py
@@ -6,6 +6,7 @@ from django.urls import reverse
 from django.utils import timezone
 
 from main.models import Experience
+from main.models import Education
 
 
 class MainTest(TestCase):
@@ -16,6 +17,13 @@ class MainTest(TestCase):
             category="internship",
         )
 
+        self.education = Education.objects.create(
+            institution="Universitas Indonesia",
+            faculty="Computer Science",
+            thumbnail="/static/img/fasilkom.png",
+            started_at=2025,
+        )
+
     def test_main_url_is_accessible(self):
         response = self.client.get(reverse("main:show_main"))
 
@@ -23,6 +31,7 @@ class MainTest(TestCase):
         self.assertTemplateUsed(response, "index.html")
         self.assertNotContains(response, self.experience.title)
         self.assertContains(response, f'href="{reverse("main:show_experience")}"')
+        self.assertContains(response, f'href="{reverse("main:show_education")}"')
 
     def test_nonexistent_page_returns_404(self):
         response = self.client.get("/halaman-yang-tidak-ada/")
@@ -34,6 +43,11 @@ class MainTest(TestCase):
         self.assertEqual(self.experience.category, "internship")
         self.assertTrue(self.experience.is_ongoing)
 
+    def test_education_model(self):
+        self.assertEqual(str(self.education), "Universitas Indonesia")
+        self.assertEqual(self.education.faculty, "Computer Science")
+        self.assertTrue(self.education.is_ongoing)
+
     def test_experience_page(self):
         response = self.client.get(reverse("main:show_experience"))
 
@@ -44,12 +58,31 @@ class MainTest(TestCase):
         self.assertContains(response, "Internship")
         self.assertContains(response, "Sedang berlangsung")
         self.assertContains(response, f'href="{reverse("main:show_main")}"')
+        self.assertContains(response, f'href="{reverse("main:show_education")}"')
+
+    def test_education_page(self):
+        response = self.client.get(reverse("main:show_education"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "education.html")
+        self.assertContains(response, self.education.institution)
+        self.assertContains(response, self.education.faculty)
+        self.assertContains(response, "Universitas Indonesia")
+        self.assertContains(response, f"{self.education.started_at} - current")
+        self.assertContains(response, f'href="{reverse("main:show_main")}"')
+        self.assertContains(response, f'href="{reverse("main:show_experience")}"')
 
     def test_empty_experience_page(self):
         Experience.objects.all().delete()
         response = self.client.get(reverse("main:show_experience"))
 
         self.assertContains(response, "Belum ada pengalaman yang ditambahkan.")
+
+    def test_empty_education_page(self):
+        Education.objects.all().delete()
+        response = self.client.get(reverse("main:show_education"))
+
+        self.assertContains(response, "Belum ada riwayat pendidikan yang ditambahkan")
 
     def test_completed_experience(self):
         self.experience.ended_at = timezone.now()
@@ -60,10 +93,20 @@ class MainTest(TestCase):
         self.assertContains(response, "Selesai")
         self.assertNotContains(response, "Sedang berlangsung")
 
+    def test_completed_education(self):
+        self.education.ended_at = timezone.now().year
+        self.education.save()
+        response = self.client.get(reverse("main:show_education"))
+
+        self.assertFalse(self.education.is_ongoing)
+        self.assertContains(response, f"{self.education.started_at} - {self.education.ended_at}")
+        self.assertNotContains(response, f"{self.education.started_at} - current")
+
+
 """
 Semua method yang namanya diawali test_ akan dijalankan otomatis oleh Django. Method setUp() dijalankan sebelum setiap test sehingga tiap test memperoleh data awal yang bersih.
 
-Enam test tersebut memeriksa hal yang berbeda:
+Sembilan test tersebut memeriksa hal yang berbeda:
 
 1.) Halaman profil dapat diakses, memakai index.html, tidak menampilkan kartu pengalaman, dan memiliki tautan ke halaman Experience.
 2.) URL yang tidak terdaftar menghasilkan status 404 Not Found.
