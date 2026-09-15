b20ac53f0710a5810a12fce20142b6b4cf1895db
parsyarifqi <parsyapetrana4@gmail.com>
2026-09-16 01:21:35 +0700

    refactor: implemented data delivery using JSON

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, show_experience, show_education, create_project, show_projects
+from main.views import show_main, show_experience, show_education, create_project, show_projects, get_projects_json, delete_project
 
 app_name = "main"
 
@@ -9,6 +9,8 @@ urlpatterns = [
     path("education/", show_education, name="show_education"),
     path("projects/", show_projects, name="show_projects"),
     path("projects/add/", create_project, name="create_project"),
+    path("api/projects/", get_projects_json, name="get_projects_json"),
+    path("projects/<uuid:project_id>/delete/",delete_project,name="delete_project"),
 ]
 
 
